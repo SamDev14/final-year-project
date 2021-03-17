edd4bae11afabfdbb87d2bf828cc685d06d72c3a
Sam Smith <samayrton95@gmail.com>
2021-03-17 16:20:19 +0000

    Add files via upload

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -133,6 +133,50 @@
             solo
           ></v-text-field>
         </v-col>
+
+                <v-col
+          cols="12"
+          sm="6"
+          md="3"
+        >
+          <v-text-field
+            label="Attack Type"
+            solo
+          ></v-text-field>
+        </v-col>
+
+        <v-col
+          cols="12"
+          sm="6"
+          md="3"
+        >
+          <v-text-field
+            label="Attack Type"
+            solo
+          ></v-text-field>
+        </v-col>
+
+        <v-col
+          cols="12"
+          sm="6"
+          md="3"
+        >
+          <v-text-field
+            label="Attack Type"
+            solo
+          ></v-text-field>
+        </v-col>
+
+                <v-col
+          cols="12"
+          sm="6"
+          md="3"
+        >
+          <v-text-field
+            label="Attack Type"
+            solo
+          ></v-text-field>
+        </v-col>
       </v-row>
     </v-container>
   </v-form>
