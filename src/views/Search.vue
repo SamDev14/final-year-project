5585cd5a36433951f67bdaeba3df8c2bb3b95a9c
SamDev14 <samayrton95@gmail.com>
2021-04-08 15:38:17 +0100

    update

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,189 +1,32 @@
 <template>
-    <v-form>
-    <v-container>
-      <v-row>
-        <v-col
-          cols="12"
-          sm="6"
-          md="3"
-        >
-          <v-text-field
-            label="Attack Type"
-            solo
-          ></v-text-field>
-        </v-col>
+  <v-card>
+    <v-tabs 
+      v-model="tab"
+      background-color="primary"
+      dark
+    >
+      <v-tab 
+        v-for="item in items" 
+        :key="item.name"
+      >
+      {{item.tab}}
+      </v-tab>
+    </v-tabs>
 
-        <v-col
-          cols="12"
-          sm="6"
-          md="3"
-        >
-          <v-text-field
-            label="Attack Type"
-            solo
-          ></v-text-field>
-        </v-col>
-
-        <v-col
-          cols="12"
-          sm="6"
-          md="3"
-        >
-          <v-text-field
-            label="Attack Type"
-            solo
-          ></v-text-field>
-        </v-col>
-
-        <v-col
-          cols="12"
-          sm="6"
-          md="3"
-        >
-          <v-text-field
-            label="Attack Type"
-            solo
-          ></v-text-field>
-        </v-col>
-
-        <v-col
-          cols="12"
-          sm="6"
-          md="3"
-        >
-          <v-text-field
-            label="Attack Type"
-            solo
-          ></v-text-field>
-        </v-col>
-
-        <v-col
-          cols="12"
-          sm="6"
-          md="3"
-        >
-          <v-text-field
-            label="Attack Type"
-            solo
-          ></v-text-field>
-        </v-col>
-
-        <v-col
-          cols="12"
-          sm="6"
-          md="3"
-        >
-          <v-text-field
-            label="Attack Type"
-            solo
-          ></v-text-field>
-        </v-col>
-
-        <v-col
-          cols="12"
-          sm="6"
-          md="3"
-        >
-          <v-text-field
-            label="Attack Type"
-            solo
-          ></v-text-field>
-        </v-col>
-
-        <v-col
-          cols="12"
-          sm="6"
-          md="3"
-        >
-          <v-text-field
-            label="Attack Type"
-            solo
-          ></v-text-field>
-        </v-col>
-
-        <v-col
-          cols="12"
-          sm="6"
-          md="3"
-        >
-          <v-text-field
-            label="Attack Type"
-            solo
-          ></v-text-field>
-        </v-col>
-
-        <v-col
-          cols="12"
-          sm="6"
-          md="3"
-        >
-          <v-text-field
-            label="Attack Type"
-            solo
-          ></v-text-field>
-        </v-col>
-
-                <v-col
-          cols="12"
-          sm="6"
-          md="3"
-        >
-          <v-text-field
-            label="Attack Type"
-            solo
-          ></v-text-field>
-        </v-col>
-
-                <v-col
-          cols="12"
-          sm="6"
-          md="3"
-        >
-          <v-text-field
-            label="Attack Type"
-            solo
-          ></v-text-field>
-        </v-col>
-
-        <v-col
-          cols="12"
-          sm="6"
-          md="3"
-        >
-          <v-text-field
-            label="Attack Type"
-            solo
-          ></v-text-field>
-        </v-col>
-
-        <v-col
-          cols="12"
-          sm="6"
-          md="3"
-        >
-          <v-text-field
-            label="Attack Type"
-            solo
-          ></v-text-field>
-        </v-col>
-
-                <v-col
-          cols="12"
-          sm="6"
-          md="3"
-        >
-          <v-text-field
-            label="Attack Type"
-            solo
-          ></v-text-field>
-        </v-col>
-      </v-row>
-    </v-container>
-  </v-form>
+    <v-tab-items v-model="tab">
+      <v-tab-item 
+        v-for="item in items"
+        :key="item.tab"
+      >
+        <v-card flat>
+          <v-card-text>{{item.content}}</v-card-text>
+        </v-card>
+      </v-tab-item>
+    </v-tab-items>
+  </v-card>
 </template>
 
 <script>
-
 export default {
   name: 'home',
 
@@ -192,7 +35,42 @@ export default {
   },
 
   data: () => ({
-    
+    tab: null,
+    items: [
+      { tab: 'Deceptive Interactions', 
+        content: '#'},
+      { tab: 'Abuse Existing Functionality', 
+        content: '#'},
+      { tab: 'Manipulate Data Structures', 
+        content: '#'},
+      { tab: 'Manipulate System Resources', 
+        content: '#'},
+      { tab: 'Code Injection', 
+        content: '#'},
+      { tab: 'Employ Probabalistic Techniques', 
+        content: '#'},
+      { tab: 'Manipulate Timing and State', 
+        content: '#'},
+      { tab: 'Collect and Analyze Information', 
+        content: '#'},
+      { tab: 'Subvert Access Control', 
+        content: '#'}
+    ],
+    checked: [],
   }),
+
+  methods: {
+    contentAdd(checked) {
+      for (i=0; i <= checked.length; i++) {
+        
+      }
+    }
+
+    contentChecked() {
+      if items.content ==  {
+        items.content.push(checked)
+      }
+    }
+  }
 };
 </script>
